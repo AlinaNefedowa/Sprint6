984a80a86ca06f55ff9b8fecc3f611136f0b4954
AlinaNefedowa <nefedowa.as@gmail.com>
2025-07-01 11:34:55 +0200

    changed page object

--- src/main/java/pageobject/ConfirmationPopup.java
+++ src/main/java/pageobject/ConfirmationPopup.java
@@ -1,4 +1,32 @@
 package pageobject;
 
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class ConfirmationPopup {
+    private WebDriver driver;
+    private WebDriverWait wait;
+    private By popupLocator = By.className("Order_ModalHeader__3FDaJ");
+    private By closeButton = By.xpath("//button[text()='Посмотреть статус']");
+
+    public ConfirmationPopup(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(5L));
+    }
+
+    public boolean isPopupVisible() {
+        this.wait.until(ExpectedConditions.visibilityOfElementLocated(this.popupLocator));
+        return this.driver.findElement(this.popupLocator).isDisplayed();
+    }
+
+    public String getPopupText() {
+        return this.driver.findElement(this.popupLocator).getText();
+    }
+
+    public void closePopup() {
+        this.driver.findElement(this.closeButton).click();
+    }
 }
--- src/main/java/pageobject/MainPage.java
+++ src/main/java/pageobject/MainPage.java
@@ -1,4 +1,26 @@
 package pageobject;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class MainPage {
+    private WebDriver driver;
+    private By topOrderButton = By.cssSelector(".Button_Button__ra12g");
+    private By bottomOrderButton = By.cssSelector(".Button_Button__ra12g.Button_Middle__1CSJM");
+
+    public MainPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void clickTopOrderButton() {
+        this.driver.findElement(this.topOrderButton).click();
+    }
+
+    public void clickBottomOrderButton() {
+        WebElement button = this.driver.findElement(this.bottomOrderButton);
+        ((JavascriptExecutor)this.driver).executeScript("arguments[0].scrollIntoView(true);", new Object[]{button});
+        button.click();
+    }
 }
